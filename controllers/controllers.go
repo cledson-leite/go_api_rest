c69cdba39b1a0d08a9e69730850eb4b289f0a4e6
cledson-leite <csbetsonline@gmail.com>
2022-06-06 13:42:34 -0300

    implementação do controle listar personalidades

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,17 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/cledson-leite/go_api_rest.git/models"
 )
 
 func Home(write http.ResponseWriter, response *http.Request)  {
 	fmt.Fprint(write, "Home page")
+}
+
+func ListarPersonalidades(write http.ResponseWriter, response *http.Request){
+	json.NewEncoder(write).Encode(models.Personalidades)
 }
--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/cledson-leite/go_api_rest.git/models"
 	"github.com/cledson-leite/go_api_rest.git/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Olá Mundo!!")
 	routes.HandleRequest()
 }
