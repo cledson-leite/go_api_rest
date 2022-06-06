14bbc4a29ca000d991524b1cfcc71680aee477c0
cledson-leite <csbetsonline@gmail.com>
2022-06-06 17:16:09 -0300

    implementar conexão com o banco

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/cledson-leite/go_api_rest.git/database"
 	"github.com/cledson-leite/go_api_rest.git/models"
 	"github.com/cledson-leite/go_api_rest.git/routes"
 )
@@ -12,7 +13,7 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
-
+	database.ConectDB()
 	fmt.Println("Olá Mundo!!")
 	routes.HandleRequest()
 }
